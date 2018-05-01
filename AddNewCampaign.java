60866cc25719a99a883cabb570be90a69bd8662c
alpcengiz3 <36951567+alpcengiz3@users.noreply.github.com>
2018-05-01 19:18:57 +0300

    Add files via upload

---
+++ AddNewCampaign.java
@@ -0,0 +1,10 @@
+package yigitersan;
+
+public class AddNewCampaign {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Client.java
@@ -0,0 +1,5 @@
+package yigitersan;
+
+public class Client {
+
+}
