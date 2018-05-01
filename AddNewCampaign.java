61d57d11e268c7450e40a6cf46b9291b7d36b51f
alpcengiz3 <36951567+alpcengiz3@users.noreply.github.com>
2018-05-01 19:21:05 +0300

    Add files via upload

---
+++ AddNewCampaign.java
@@ -0,0 +1,14 @@
+package yigitersan;
+
+public class AddNewCampaign {
+
+	public void showClientCampaigns() {
+		System.out.println("Clients are here..");
+		}
+		public void createNewCampaign() {
+			
+			System.out.println("New campaigns are created here");
+
+	}
+
+}
